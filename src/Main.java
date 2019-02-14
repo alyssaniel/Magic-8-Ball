2555910adf07d5ee21fae1b99fb88ead807640dd
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-02-14 08:28:20 -0500

    Put the imports and Scanners in

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        
+
+        Scanner keyboard;
+        String userName;
+
+
+
+
+
+
+
     }
 }
