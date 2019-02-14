75e354b6fced848aea1bbc39e6c79066d211a70d
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-02-14 08:34:26 -0500

    Put in the first line of code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
-        Scanner keyboard;
+        // write your code here
         String userName;
+        Scanner keyboard;
+
+
+        //initializing
+        keyboard= new Scanner(System.in);
+
+
+        System.out.println("What is your name?");
+        userName = keyboard.nextLine();
+
 
 
 
