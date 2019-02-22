43e702c19e285d964279694d835d284418f95ae1
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-02-22 07:46:31 -0500

    Starting the user code/Collecting the users input

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,33 @@ public class Main {
         String userName;
         Scanner keyboard;
         int go;
-
+        String userInput;
         //initializing
         keyboard= new Scanner(System.in);
         Random generator  = new Random();
-
+        go = 0;
+        while (go == 0)
 
         System.out.println("What is your name?");
         userName = keyboard.nextLine();
 
-        System.out.println("Ask me only yes or no Questions!");
+        System.out.println("Ask me only yes or no Questions!(Type Ok if you understand)");
+        userInput = keyboard.nextLine();
         System.out.println(""+userName+", You May Ask Me A Question When You Are Ready!");
+
+        String answer = keyboard.nextLine();
+        int choice = 1 + generator.nextInt(10);
+        String response = "";
+        if (choice == 1) 
+            response = "Nope, never!";
+
+
+
+
+
+
+
+
 
 
 
